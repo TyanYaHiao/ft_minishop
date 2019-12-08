a654a032c4ea7d03bd41b6cded1c8e61100b4b44
Dvicto <artempr15@gmail.com>
2019-12-08 12:56:27 +0300

    back

--- html/modif.php
+++ html/modif.php
@@ -1,14 +1,16 @@
 <?php
 	$array = [];
 	
-	
-	if (!(file_exists("../ex01/private"))) {
-		exit("ERROR\n");
-	}
+	if ($_POST['submit'] == 'GO BACK'){
+		include ('login.html');
+		exit();
+		}
 	if (!($_POST['newpasswd']) || !($_POST['login']) || !($_POST['submit'] == 'OK') || !($_POST['oldpasswd'])) {
+		include('modifagain.html');
 		exit("Error\n");
 	}
-	if (!($array = unserialize(file_get_contents("../ex01/private/passwd")))){
+	if (!($array = unserialize(file_get_contents("../private/passwd")))){
+		include('modifagain.html');
 		exit("ERROR\n");
 	}
 	foreach ($array as &$value)
@@ -16,12 +18,15 @@
 		if ($value["login"] == $_POST["login"]) {
 			if ($value["passwd"] == hash("whirlpool",$_POST["oldpasswd"])) {
 				$value["passwd"] = hash("whirlpool",$_POST["newpasswd"]);
-				file_put_contents("../ex01/private/passwd", serialize($array));
+				file_put_contents("../private/passwd", serialize($array));
+				include('login.html');
 				exit("OK\n");
 			} else {
 				echo "ERROR\n";
+				include('modifagain.html');
 				exit ();
 			}
 		}
 	}
+	include('modifagain.html');
 	exit("ERROR\n");
