c468dc16d265a5c48cc44303104d9fa7bedc40ba
Dvicto <artempr15@gmail.com>
2019-12-08 19:43:50 +0300

    private !

--- html/login.php
+++ html/login.php
@@ -3,14 +3,12 @@
 	session_start();
 	if (($_POST['login'] && $_POST['passwd']) && auth($_POST['login'], $_POST['passwd']))
 	{
-		echo "OK\n";
 		$_SESSION['loggued_on_user'] = $_POST['login'];
-		include ("");
+		header('location: index.php');
 		exit;
 	}
 	else if (($_POST['login'] && $_POST['passwd']))
 	{
-		echo "ERROR\n";
 		include ('loginagain.html');
 		exit;
 	}
--- html/logout.php
+++ html/logout.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
 if ($_SESSION['loggued_on_user'])
-	$_SESSION['loggued_on_user'] = NULL;
+	$_SESSION['loggued_on_user'] = "";
+	header('location: index.php');
 ?>
